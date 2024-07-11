da2813b2f0856c85f83d665a4abfaae95bf8efbe
Ananggadipa Raswanto <dipovespo@gmail.com>
2024-07-11 19:23:38 +0700

    Add files via upload

---
+++ razer restart.bat
@@ -0,0 +1,12 @@
+@echo off
+echo Closing Razer Synapse...
+taskkill /F /IM "Razer Synapse 3.exe"
+
+echo Waiting for 3 seconds...
+timeout /t 3
+
+echo Starting Razer Synapse...
+start "" "C:\Program Files (x86)\Razer\Synapse3\WPFUI\Framework\Razer Synapse 3 Host\Razer Synapse 3.exe"
+
+echo Done.
+pause

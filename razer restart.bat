4dac96e6aa2a5f39b130bdf85730f4eef7dac1a7
Ananggadipa Raswanto <dipovespo@gmail.com>
2024-07-11 19:27:40 +0700

    Update razer restart.bat

--- razer restart.bat
+++ razer restart.bat
@@ -8,5 +8,7 @@ timeout /t 3
 echo Starting Razer Synapse...
 start "" "C:\Program Files (x86)\Razer\Synapse3\WPFUI\Framework\Razer Synapse 3 Host\Razer Synapse 3.exe"
 
-echo Done.
-pause
+echo Done. Closing in 3 seconds..
+
+timeout /t 3
+exit
